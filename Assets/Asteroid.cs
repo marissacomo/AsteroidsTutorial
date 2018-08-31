09b9676ba70649690d0881097d9a8392d781c222
marissacomo <mcomo@friars.providence.edu>
2018-08-31 15:03:56 -0400

    asteroids completed

---
+++ Assets/Asteroid.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour {
+
+    // Use this for initialization
+    public int pointValue;
+    void Start()
+    {
+        pointValue = 10;
+    }
+
+    // Update is called once per frame
+    void Update () {	
+	}
+
+    public GameObject deathExplosion;
+    public AudioClip deathKnell;
+    public void Die()
+    {
+        AudioSource.PlayClipAtPoint(deathKnell, gameObject.transform.position);
+        Instantiate(deathExplosion, gameObject.transform.position,Quaternion.AngleAxis(-90, Vector3.right));
+        GameObject obj = GameObject.Find("GlobalObject");
+        Global g = obj.GetComponent<Global>();
+        g.score += pointValue;
+        Destroy(gameObject);
+    }
+}
